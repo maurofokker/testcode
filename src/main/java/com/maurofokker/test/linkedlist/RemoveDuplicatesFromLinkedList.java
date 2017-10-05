ff66779419ec2c1ceb50e5f34e121725a456732c
maurofokker <mgaldames@gmail.com>
2017-10-05 00:25:46 -0300

    add 2 linkedlist problems

---
+++ src/main/java/com/maurofokker/test/linkedlist/RemoveDuplicatesFromLinkedList.java
@@ -0,0 +1,13 @@
+package com.maurofokker.test.linkedlist;
+
+/**
+ * Created by mauro on 10/5/17.
+ *
+ * Remove duplicate nodes from linked list of integers while keeping only the first occurrence of duplicates
+ *
+ * i.e.
+ *  original linkedlist: head -> 7 -> 14 -> 28 -> 28 -> 14 -> 21 -> null
+ *  returned linkedlist: head -> 7 -> 14 -> 28 -> 21 -> null
+ */
+public class RemoveDuplicatesFromLinkedList {
+}
---
+++ src/main/java/com/maurofokker/test/linkedlist/ReverseSinlgyLinkedList.java
@@ -0,0 +1,20 @@
+package com.maurofokker.test.linkedlist;
+
+
+/**
+ * Created by mauro on 10/4/17.
+ *
+ * Given the pointer/reference to the head of a singly linked list, reverse it and return the pointer/reference
+ * to the head of reversed linked list.
+ *
+ * i.e.
+ *  original linkedlist: head -> 7 -> 14 -> 21 -> 28 -> null
+ *  reversed linkedlist: reversed/head -> 28 -> 21 -> 14 -> 7 -> null
+ */
+public class ReverseSinlgyLinkedList {
+
+    public void reverseLinkedListIterative(Integer head) {
+
+    }
+
+}
